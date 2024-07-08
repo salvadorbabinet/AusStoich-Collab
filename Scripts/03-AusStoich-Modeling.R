5d3110ad47ce1c95454d2055d8074a7d599f4800
Salvador Babinet <salvador.babinet@mail.mcgill.ca>
2024-07-08 14:23:44 -0400

    Import working dataset

--- Scripts/03-AusStoich-Modeling.R
+++ Scripts/03-AusStoich-Modeling.R
@@ -3,3 +3,15 @@
 library(here)
 library(tidyverse)
 library(tidymodels)
+
+# Data import
+data <- read_csv(
+  file = here('Inputs', 'AusStoich_merged_final.csv'),
+  na = c('', 'NA', 'uncertain'),
+  col_types = cols(
+    woodiness = col_factor(c('0', '1')),
+    reclass_life_history = col_factor(c('short', 'long')),
+    putative_BNF = col_factor(c('0', '1')),
+    myc_type = col_factor(c('AM', 'EcM', 'EcM-AM', 'ErM', 'NM', 'NM-AM'))
+    )
+  )
