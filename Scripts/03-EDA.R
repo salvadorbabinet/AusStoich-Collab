637f1603bc339e42cb56b3bb1f1fc607c9d2edb5
salvadorbabinet <salvador.babinet@mail.mcgill.ca>
2024-08-02 14:33:53 -0400

    Missing environmental stuff

--- Scripts/03-EDA.R
+++ Scripts/03-EDA.R
@@ -24,7 +24,7 @@ summarize_cont <- function(data, variable, grouping = NULL) {
   )
 }
 
-# Investigating missing data 
+# Missing data ------------------------------------------------------------
 # All entries with unexpected NAs (not C and P or ratios)
 missing <- aus_data |> filter(if_any(!leaf_P_per_dry_mass:CP_ratio, is.na))
 missing |> # Print unexpected column names with NAs 
@@ -32,6 +32,17 @@ missing |> # Print unexpected column names with NAs
   pivot_longer(everything()) |> 
   filter(value > 0) |> 
   print(n = Inf)
+
+# Soil 
+missing_soil <- missing |> filter(is.na(SN_total_0_30)) # Hayes_2014 and EsperonRodriguez_2020 
+missing_soil |> select(c(observation_id, species_binom, lat_deg:long_deg, SN_total_0_30:AP_total_0_30)) |> 
+  print(n = Inf)
+aus_data |> filter(dataset_id == 'EsperonRodriguez_2020') # Both datasets have more entries than these NA ones 
+
+# NPP and climate 
+missing_seasonality <- missing |> filter(is.na(NPP)) # All from Hayes_2014 
+missing_seasonality |> select(c(observation_id, species_binom, lat_deg:long_deg, NPP:temp_seasonality))
+aus_data |> filter(dataset_id == 'Hayes_2014') # But many more Hayes_2014 entries w/ seasonality 
 
 
 # Categorical variables 
