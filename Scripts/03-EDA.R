6205de158be196f37a6ee2c92c9d8dd42e93f9cd
salvadorbabinet <salvador.babinet@mail.mcgill.ca>
2024-09-12 15:53:05 -0400

    More density plots, start (very rough) log plots, Andrew code walk through.

--- Scripts/03-EDA.R
+++ Scripts/03-EDA.R
@@ -3,6 +3,7 @@
 library(here)
 library(dplyr)
 library(ggplot2)
+theme_set(theme_bw())
 library(corrplot)
 library(patchwork)
 
@@ -55,6 +56,44 @@ dot_plot_by_family <- function(data = aus_data, xvar, yvar) {
       se = FALSE) +
     theme_bw()
   p1 + p2
+}
+
+log_plot_by_family <- function(data = aus_data, xvar, yvar) {
+  p1 <- ggplot(data, aes(x = {{xvar}}, y = {{yvar}})) +
+    geom_point(alpha = 0.2, size = 0.6) +
+    geom_smooth(formula = log(y) ~ log(x), se = FALSE, linetype = "dashed") +
+    geom_abline(intercept = 0, slope = 1) +
+    coord_trans(x = "log", y = "log") +
+    theme_bw()
+  p2 <- ggplot(mapping = aes(x = {{xvar}}, y = {{yvar}})) +
+    geom_point(
+      data = filter(data, !family %in% c("Myrtaceae", "Fabaceae", "Proteaceae")),
+      alpha = 0.2,
+      size = 0.6) +
+    geom_smooth(data = data, formula = log(y) ~ log(x), linetype = "dashed", se = FALSE) +
+    geom_point(
+      data = filter(data, family %in% c("Myrtaceae", "Fabaceae", "Proteaceae")),
+      mapping = aes(color = family),
+      alpha = 0.2,
+      size = 0.6) +
+    geom_smooth(
+      data = filter(data, family %in% c("Myrtaceae", "Fabaceae", "Proteaceae")),
+      mapping = aes(color = family),
+      formula = log(y) ~ log(x),
+      se = FALSE) +
+    geom_abline(intercept = 0, slope = 1) +
+    coord_trans(x = "log", y = "log") +
+    theme_bw()
+  p1 + p2
+}
+
+density_plot_by_family <- function(data = aus_data, xvar, xlim = NULL, ylim = NULL) { # nolint: line_length_linter.
+  ggplot(
+    data = filter(data, family %in% c("Myrtaceae", "Fabaceae", "Proteaceae")),
+    mapping = aes(x = {{xvar}}, color = family, fill = family)
+  ) +
+  geom_density(alpha = 0.4, linewidth = 0.7) +
+  coord_cartesian(xlim = xlim, ylim = ylim)
 }
 
 
@@ -159,11 +198,59 @@ ggplot(
 # Investigate relationships across major families
 # Foliar to environmental elements
 dot_plot_by_family(xvar = SN_total_0_30, yvar = leaf_N_per_dry_mass)
+density_plot_by_family(xvar = SN_total_0_30)
+
 dot_plot_by_family(xvar = SP_total_0_30, yvar = leaf_P_per_dry_mass)
+density_plot_by_family(xvar = SP_total_0_30, xlim = c(0, 0.1))
+
 dot_plot_by_family(xvar = AP_total_0_30, yvar = leaf_P_per_dry_mass)
-dot_plot_by_family(xvar = SOC_total_0_30, yvar = leaf_C_per_dry_mass)
+density_plot_by_family(xvar = AP_total_0_30)
 
 # Foliar elements to each other
 dot_plot_by_family(xvar = leaf_P_per_dry_mass, yvar = leaf_N_per_dry_mass)
+log_plot_by_family(xvar = leaf_P_per_dry_mass, yvar = leaf_N_per_dry_mass)
 dot_plot_by_family(xvar = leaf_C_per_dry_mass, yvar = leaf_N_per_dry_mass)
 dot_plot_by_family(xvar = leaf_C_per_dry_mass, yvar = leaf_P_per_dry_mass)
+
+density_plot_by_family(xvar = leaf_N_per_dry_mass)
+density_plot_by_family(xvar = leaf_P_per_dry_mass)
+density_plot_by_family(xvar = leaf_C_per_dry_mass)
+
+
+# Andrew demonstration
+data <- sample(c(0, 1), size = 500, replace = T)
+mean(data)
+sd(data)
+
+nested_aus_data <- aus_data |> nest_by(family) |>
+  mutate(
+    row_count = nrow(data), 
+    mean_sn = mean(data$SN_total_0_30), 
+    data_c = list(
+      mutate(data, SN_total_0_30 = SN_total_0_30 - mean_sn)
+    )) |> 
+  filter(row_count > 2) |> 
+  mutate(
+    models = list(lm(formula = leaf_N_per_dry_mass ~ SN_total_0_30, data = data_c)),
+    tidymodels = list(broom::tidy(models))
+  )
+
+nested_aus_data |> select(-c(data, models)) |> tidyr::unnest(tidymodels) |> 
+  mutate(family = forcats::fct_reorder(family, estimate, .fun = min)) |>
+  ggplot(mapping = aes(x = family, y = estimate, ymin = estimate - std.error, ymax = estimate + std.error)) + 
+  geom_pointrange() + 
+  facet_wrap(~term, scales = "free")
+
+nested_aus_data |> select(-c(data, models)) |> tidyr::unnest(tidymodels) |> 
+  select(family, term, estimate) |> 
+  tidyr::pivot_wider(names_from = term, values_from = estimate) |>
+  ggplot(mapping = aes(x = `(Intercept)`, y = SN_total_0_30)) + 
+  geom_point()
+
+nested_aus_data |> select(-c(data, models)) |> tidyr::unnest(tidymodels) |> 
+  select(family, term, estimate) |> 
+  tidyr::pivot_wider(names_from = term, values_from = estimate) |>
+  ggplot(mapping = aes(x = `(Intercept)`)) + 
+  geom_histogram()
+
+nested_aus_data$tidymodels[[2]]
